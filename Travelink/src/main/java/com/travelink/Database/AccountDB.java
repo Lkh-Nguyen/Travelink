71d67314502dae6c820b7e7a38ed1319ab266b38
MeabuHC <woolythesheepnew@gmail.com>
2024-06-04 09:30:48 +0700

    Update AccountDB.java

--- Travelink/src/main/java/com/travelink/Database/AccountDB.java
+++ Travelink/src/main/java/com/travelink/Database/AccountDB.java
@@ -17,21 +17,6 @@ import java.sql.SQLException;
  */
 public class AccountDB implements DatabaseInfo {
 
-    public static Connection getConnect() {
-        try {
-            Class.forName(DRIVERNAME);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Error loading driver" + e);
-        }
-        try {
-            Connection con = DriverManager.getConnection(DBURL, USERDB, PASSDB);
-            return con;
-        } catch (SQLException e) {
-            System.out.println("Error: " + e);
-        }
-        return null;
-    }
-
     public static boolean insertAccount(Account Account) {
         Connection con = DatabaseInfo.getConnect();
         try {

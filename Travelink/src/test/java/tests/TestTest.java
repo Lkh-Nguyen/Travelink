c30c54b534fc5f34e33d2c8d7005a98092591f0e
hoanghai1910 <142432287+hoanghai1910@users.noreply.github.com>
2024-07-19 10:22:42 +0700

    up

--- Travelink/src/test/java/tests/TestTest.java
+++ Travelink/src/test/java/tests/TestTest.java
@@ -1,38 +1,39 @@
 package tests;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
-import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestTest {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
+
   @BeforeEach
   public void setUp() {
-    driver = new ChromeDriver();
+    // Use WebDriverManager to setup the EdgeDriver
+    WebDriverManager.edgedriver().setup();
+
+    driver = new EdgeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
+
   @AfterEach
   public void tearDown() {
     driver.quit();
   }
+
   @Test
   public void test() {
     driver.get("http://localhost:8080/Travelink/homeCustomerServlet");
